7292bb2a2b77581e1cf7398d420d1201301424c7
isti481 <95468363+isti481@users.noreply.github.com>
2022-06-19 22:25:56 +0200

    admin2.php táblázat rendbetétele
    admin2.php táblázat rendbetétele

--- PHP MySql műveletek/admin2.php
+++ PHP MySql műveletek/admin2.php
@@ -4,120 +4,66 @@ $servername="localhost";
 $username="root";
 $password="";
 $database="felhasznalok";
-
-
 $conn = new mysqli($servername, $username, $password, $database);
 $conn->set_charset("utf8");
-
 if ($conn->connect_error) {
   die("Kapcsolat hiba: " . $conn->connect_error);
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Admin</title>    
+<title>Admin</title> 
+<style>
+table tr :hover {background-color: yellow;}
+        table {
+            border-collapse: collapse;  
+        }
+        th {
+          border: 1px solid black;
+            text-align: left;
+            padding: 5px;
+        }            
+        table tr, td {
+          text-align: center;
+            border: 1px solid black;
+            padding: 10px;                       
+        }
+        tr :nth-child(even){
+            background-color: lightgreen;
+        }  
+</style>  
 </head>
-<!--
 <body>
-<table>
-<tr>
-<td></td>
-</tr>
-<tr>
-<td></td>
-</tr>
-<tr>
-<td></td>
-</tr>
-
-
-</table>
--->
-
-
-
-
-
-
 <?php
-/*
-$nev=$_POST["nev"];      
-$email=$_POST["email"];
-$tel = ($_POST["tel"] == "" ? "Nem adott meg számot" : $_POST["tel"]);
-
-$password=$_POST["jelszo"];
-$passwordsha=sha1($password);
-
-$stat=$_POST["0"];
-
-$sql="INSERT INTO adatok(nev,email,tel,password,stat) VALUES ('$nev', '$email', '$tel', '$passwordsha', '$stat')";
-
-if ($conn->query($sql)===TRUE) {
-  echo '
-  <script>
-  alert("Felhasznalo sikeresen hozzáadva!")
-  </script>';
-}
-else{
-  echo "Hiba: ".$sql."<br>". $conn->error;
-}
-
-$conn->close();
-
-*/
-
-
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
 $sql = "SELECT nev, email, tel, stat FROM adatok";
 $result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-  echo "<table><tr><th>Név: </th><th>Telefonszám:  </th><th>Státusz: </th></tr>";
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<tr>Név:<td> " . $row["nev"]. " </tr></td><tr><td>Telefonszám: " . $row["tel"]. "Státusz: " . $row["stat"]. "<br>";
-  }
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "--------------------------------------------------------------------------------";
-    echo "</table>";
-  
-  echo"<table>
-  <tr>
-  <td>Név:</td>
-  </tr>
-  <tr>
-  <td>".$row["nev"]."</td>
-  </tr>
-  <tr>
-  <td>Telefonszám</td>
-  </tr>
-  <tr>
-  <td></td>
-  <tr>
-  <td></td>
-  </tr>
-  </tr>
-  </table>";
-  } 
+if (mysqli_num_rows($result) > 0) {   
+    echo "<table>
+        <tr>
+    <th>Név</th>
+    <th>Email</th>
+    <th>Telefonszám</th>
+    <th>Státusz</th>
+    </tr>";   
+    while($row = mysqli_fetch_assoc($result)) {
+    echo " <tr>";
+    echo "<td>" . $row['nev'] . "</td>";
+    echo "<td>" . $row['email'] . "</td>";
+    echo "<td>" . $row['tel'] . "</td>";  
+    echo "<td>" . $row['stat'] . "</td>";
+    echo " </tr>";}
+    echo " </table>";  
 } else {
   echo "0 results";
 }
-
 mysqli_close($conn);
-
-
-
-
-
 ?>
 </body>
 </html>
